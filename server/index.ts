7254e10e8eb224fe69a4e027efa136c8f7118968
Hassene Benammou <hassene.benammou@gmail.com>
2025-03-15 00:34:53 -0400

    dist

--- server/index.ts
+++ server/index.ts
@@ -58,10 +58,10 @@ app.use((req, res, next) => {
 
   // ALWAYS serve the app on port 5000
   // this serves both the API and the client
-  const port = 5000;
+  const port = 5005;
   server.listen({
     port,
-    host: "0.0.0.0",
+    host: "localhost",// "0.0.0.0",
     reusePort: true,
   }, () => {
     log(`serving on port ${port}`);
